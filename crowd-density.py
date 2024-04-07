d0e87a7d58ade4e6f5bad0262d4e50f60bfe7d55
Thomas Nadin-Hepburn <tom.nadin.hepburn@gmail.com>
2024-04-07 14:41:53 +0100

    starting work on transfomer

--- crowd-density.py
+++ crowd-density.py
@@ -2,6 +2,7 @@
 from evaluation import evaluate
 from CNNunet import UNet
 from CNNunettrain import train_model
+from TransUNet import TransUNet
 import torch
 from sklearn.model_selection import train_test_split
 from dataset import getImgH5, CustomDataset
@@ -11,6 +12,7 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
+    parser.add_argument('--modeltype', '-t', metavar='T', type=str, default='CNN', help='Type of model to use. (CNN, TRANS)')
     parser.add_argument('--evaluation', '-v', metavar='EVAL', type=bool, default=False, help='Evaulate on loaded model')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
 
@@ -34,16 +36,24 @@ if __name__ == '__main__':
     val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = UNet(n_channels=3,n_classes=1,bilinear=True)
+    if args.modeltype == 'CNN':
+        model = UNet(n_channels=3,n_classes=1,bilinear=True)
 
-    if args.load:
-        model.to(device=device)
-        state_dict = torch.load(args.load, map_location=device)
-        model.load_state_dict(state_dict)
+        if args.load:
+            model.to(device=device)
+            state_dict = torch.load(args.load, map_location=device)
+            model.load_state_dict(state_dict)
 
-    if args.evaluation:
-        mae_result = evaluate(model, val_data=val_loader)
-        print(mae_result)
+        if args.evaluation:
+            mae_result = evaluate(model, val_data=val_loader)
+            print(mae_result)
 
-    elif not args.evaluation:
-        train_model(model=model, epochs=10, train_dataset=train_loader, val_dataset=val_loader, device=device, n_train=len(x_train))
+        elif not args.evaluation:
+            train_model(model=model, epochs=10, train_dataset=train_loader, val_dataset=val_loader, device=device, n_train=len(x_train))
+
+        
+    elif args.modeltype == 'TRANS':
+        model = TransUNet(device)
+
+        if args.load:
+            model.load_model(args.load)
